98625ee4629b603adfdbf866a017e97a014cb68c
Hugo van Galen <hugo@wellevue.com>
2023-06-02 15:09:47 +0200

    Bump version.

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define KOMPLEMENT_VERSION "0.1alpha3"
-#define KONFIGURE_VERSION  "0.1alpha3"
+#define KOMPLEMENT_VERSION "0.1alpha4"
+#define KONFIGURE_VERSION  "0.1alpha4"
 
 #define RISK_DISCLAIMER     "** DISCLAIMER: YOU USE THIS AT YOUR OWN RISK **\n"\
         "I am not responsible if this utility screws things up. I did my best to test this\n"\
