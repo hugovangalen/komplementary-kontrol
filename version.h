e5e7d0a392c118c0b0adddb0031dee1bbc99847f
Hugo van Galen <hugo@wellevue.com>
2023-06-05 19:56:05 +0200

    Bumped version.

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define KOMPLEMENT_VERSION "0.1alpha6"
+#define KOMPLEMENT_VERSION "0.1alpha7"
 #define KONFIGURE_VERSION  "0.1alpha5"
 
 #define RISK_DISCLAIMER     "** DISCLAIMER: YOU USE THIS AT YOUR OWN RISK **\n"\
